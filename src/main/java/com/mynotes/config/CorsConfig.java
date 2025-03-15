0adbbc19012ede4d98d8cea6b5c7963d167cea53
abjeet16 <avjeetyadav00@gmail.com>
2025-03-15 14:12:24 +0530

    entity changes

--- src/main/java/com/mynotes/config/CorsConfig.java
+++ src/main/java/com/mynotes/config/CorsConfig.java
@@ -9,10 +9,11 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*") // Allows all external origins
+                .allowedOriginPatterns("*")  // Allows all origins
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
 }
+
 
